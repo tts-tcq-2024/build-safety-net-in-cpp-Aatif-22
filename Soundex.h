5b0fe9b016a5ba53ea896caac6606dbef186bb30
Aatif-22 <mdaatifshafique54@gmail.com>
2024-09-18 14:43:37 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,15 +1,8 @@
-#ifndef SOUNDEX_HPP
-#define SOUNDEX_HPP
-
-#include <string> 
-
-class Soundex {
-public:
-    static std::string generateSoundex(const std::string& name); 
-
-private:
-    static char getMappedSoundexCode(char c);
-    static bool SoundexCodeCheck(char code, char prevCode);
-};
-
-#endif // SOUNDEX_HPP
+#ifndef SOUNDEX_H
+#define SOUNDEX_H
+ 
+#include <string>
+ 
+std::string generateSoundex(const std::string& name);
+ 
+#endif // SOUNDEX_H
